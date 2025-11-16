56fb3ba4baec8815b70b00bd61fd09af7b9da6d1
Krishna Pratap Singh Chauhan <145362216+krishnapschauhan@users.noreply.github.com>
2025-11-16 13:34:10 +0530

    Replace local image imports with Cloudinary URLs
    Updated asset imports to use Cloudinary URLs for images.

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,13 +1,4 @@
 import logo from './logo.png'
-import hero_img from './hero_img.jpg'
-import img1 from './img1.jpg'
-import img2 from './img2.jpg'
-import img3 from './img3.jpg'
-import img4 from './img4.jpg'
-import img5 from './img5.jpg'
-import img6 from './img6.jpg'
-import img7 from './img7.jpg'
-import img8 from './img8.jpg'
 import cart_icon from './cart_icon.png'
 import bin_icon from './bin_icon.png'
 import dropdown_icon from './dropdown_icon.png'
@@ -19,23 +10,25 @@ import star_dull_icon from './star_dull_icon.png'
 import star_icon from './star_icon.png'
 import support_img from './support_img.png'
 import menu_icon from './menu_icon.png'
-import about_img from './about_img.jpg'
-import contact_img from './contact_img.jpg'
 import razorpay_logo from './razorpay_logo.png'
 import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
 
 export const assets = {
     logo,
-    hero_img,
-    img1,
-    img2,
-    img3,
-    img4,
-    img5,
-    img6,
-    img7,
-    img8,
+    hero_img: "https://res.cloudinary.com/dqumagamd/image/upload/v1763279563/hero_img_bdvea3.jpg",
+    img1: "https://res.cloudinary.com/dqumagamd/image/upload/v1763279570/img1_tfjgut.jpg",
+    img2: "https://res.cloudinary.com/dqumagamd/image/upload/v1763279578/img2_h1mogc.jpg",
+    img3: "https://res.cloudinary.com/dqumagamd/image/upload/v1763279571/img3_hjiqx4.jpg",
+    img4: "https://res.cloudinary.com/dqumagamd/image/upload/v1763279854/img4_s0dbs6.jpg",
+    img5: "https://res.cloudinary.com/dqumagamd/image/upload/v1763279564/img5_lzmeoi.jpg",
+    img6: "https://res.cloudinary.com/dqumagamd/image/upload/v1763279572/img6_zl8nxn.jpg",
+    img7: "https://res.cloudinary.com/dqumagamd/image/upload/v1763279854/img7_bkddiq.jpg",
+    img8: "https://res.cloudinary.com/dqumagamd/image/upload/v1763279569/img8_ifdcvv.jpg",
+
+    about_img: "https://res.cloudinary.com/dqumagamd/image/upload/v1763279854/about_img_cdhi1b.jpg",
+    contact_img: "https://res.cloudinary.com/dqumagamd/image/upload/v1763279558/contact_img_yigx4f.jpg",
+    
     cart_icon,
     dropdown_icon,
     exchange_icon,
@@ -47,8 +40,6 @@ export const assets = {
     bin_icon,
     support_img,
     menu_icon,
-    about_img,
-    contact_img,
     razorpay_logo,
     stripe_logo,
     cross_icon
